648893e7f44cb0788a2e5e4da39a17c404a06155
Rober <roberdz30@gmail.com>
2024-03-03 18:19:21 -0300

    deploy

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,25 +1,28 @@
-const express = require("express")
-const cors = require("cors")
-const morgan = require("morgan")
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const mainRouter = require("./routes/mainRouters");
 
-const cookieParser = require("cookie-parser")
-const mainRouter = require("./routes/mainRouters")
+const app = express();
 
-const app = express()
+app.use(cors({
+  origin: "*", // o específica el dominio permitido en lugar de "*"
+  credentials: true,
+}));
 
-app.use(cors())
-app.use(express.urlencoded({ extended: true, limit: "50mb" }))
-app.use(express.json({ limit: "50mb"}))
-app.use(cookieParser())
-app.use(morgan("dev"))
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
 
-app.use("/", mainRouter)
+app.use("/", mainRouter);
 
-app.use((err, req, res, next ) => {
-    const status = err.status || 500;
-    const message = err.message || "Internal app Error";
-    console.error(err);
-    res.status(status).send(message)
-})
+app.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
 module.exports = app;
