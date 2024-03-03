9c0354629025c2b16fd1705e000ee00dd133581f
Rober <roberdz30@gmail.com>
2024-03-03 17:52:16 -0300

    ahora2

--- BackEnd/src/db.js
+++ BackEnd/src/db.js
@@ -2,16 +2,22 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize")
 const fs = require("fs")
 const path = require("path")
-const pg = require('pg');
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 
 const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+    DB_DEPLOY,
+
+    //`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+
     {
         logging: false,
         native: false,
-        dialectModule: pg
+        dialectOptions: {
+            ssl: {
+                require: true
+            }
+        }
     }
 )
 
